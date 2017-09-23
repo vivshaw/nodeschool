08f59da594aa74d25fcb9946b217cbdbe84dfc97
vivshaw <hvivianshaw@gmail.com>
2017-09-23 02:21:33 -0400

    implement exercise 5

--- elementary-electron/index.js
+++ elementary-electron/index.js
@@ -1,7 +1,28 @@
 const picture = require('cat-picture');
 const image = require('lightning-image-poly');
+const remote = require('electron').remote;
+const fs = require('fs');
 
+/* Remove the old picture */
+picture.remove();
+
+/* Set up our visualization */
 const src = picture.src;
 const viz = new image('#visualization', null, [src], {hullAlgorithm: 'convex'});
 
-picture.remove();
+/* Save screenshot to PDF */
+const savePdf = () => {
+    remote.getCurrentWebContents().printToPDF({
+        portrait: true
+    }, (err, data) => {
+        fs.writeFile('annotation.pdf', data, err => {
+            if (err) alert('error generating pdf! ' + err.message);
+            else alert('pdf saved!');
+      })
+    })
+}
+
+/* Listen for the P key & save a screenshot on keydown */
+window.addEventListener('keydown', event => {
+    if (event.keyCode == 80) savePdf();
+})
