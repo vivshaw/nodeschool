5652876ded15fb0f3456ced71b3babd7def276c4
vivshaw <hvivianshaw@gmail.com>
2017-09-23 03:44:38 -0400

    implement exercise 2

---
+++ introtowebgl/assets/BoilerplateApp/scripts/app.js
@@ -0,0 +1,64 @@
+var demo = (function(){
+
+    "use strict";
+    
+    var scene=new THREE.Scene(),
+        light= new THREE.AmbientLight(0xffffff),
+        renderer,
+        camera,
+        renderer = new THREE.WebGLRenderer(),
+        box,
+        ground,
+        controls=null,
+        swap=1;
+
+        function initScene(){
+    
+            renderer.setSize( window.innerWidth, window.innerHeight );
+            document.getElementById("webgl-container").appendChild(renderer.domElement);
+
+            scene.add(light);
+                      
+            camera = new THREE.PerspectiveCamera(
+                    35,
+                    window.innerWidth / window.innerHeight,
+                    1,
+                    1000
+                );
+            
+            camera.position.set( 0, 0, 100 );
+            
+            scene.add(camera);  
+
+            box = new THREE.Mesh(
+              new THREE.CubeGeometry(
+                20,
+                20,
+                20),
+              new THREE.MeshBasicMaterial({color: 0xFF0000}));
+
+            scene.add(box);
+
+            requestAnimationFrame(render);
+
+        };
+
+        function render() {
+                renderer.render(scene, camera); 
+                
+                camera.position.z -= 1 * swap;
+
+                if (camera.position.z < 25 || camera.position.z > 100) {
+                    swap = -swap;
+                }
+
+                box.rotation.x += .1;
+                box.rotation.y += .01;
+                box.rotation.z += .05;
+                
+                requestAnimationFrame(render);
+        };
+       
+        window.onload = initScene;
+
+})();
