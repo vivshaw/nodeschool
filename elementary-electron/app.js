0ea3a7cd13d072508845b327b2cc2ea15e806fea
vivshaw <hvivianshaw@gmail.com>
2017-09-23 02:18:20 -0400

    implement exercise 2

---
+++ elementary-electron/app.js
@@ -0,0 +1,6 @@
+const electron = require('electron');
+
+electron.app.on('ready', () => {
+    const mainWindow = new electron.BrowserWindow({width: 600, height: 800});
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+})
